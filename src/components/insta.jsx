82c436389a9fa14444e0649eb2e2f81c15540d0e
Nirmal <akoliyanirmal43@gmail.com>
2024-07-21 21:21:54 +0530

    completed insta page

--- src/components/insta.jsx
+++ src/components/insta.jsx
@@ -1,86 +1,59 @@
 import React from "react";
 import Image from "next/image";
 
-import instaImg1 from "../assets/images/insta1.png";
-import instaImg2 from "../assets/images/insta2.png";
-import instaImg3 from "../assets/images/insta3.png";
-import instaImg4 from "../assets/images/insta4.png";
-import instaImg5 from "../assets/images/insta5.png";
-import instaImg6 from "../assets/images/insta6.png";
-import instaImg7 from "../assets/images/insta7.png";
-import instaImg8 from "../assets/images/insta8.png";
-import instaIcon from "../assets/icons/instaIcon.png";
-import Link from "next/link";
+import photo1 from "../assets/images/photo1.jpg";
+import photo2 from "../assets/images/photo2.jpg";
+import photo3 from "../assets/images/photo3.jpg";
+import photo4 from "../assets/images/photo4.jpg";
+import photo5 from "../assets/images/photo5.jpg";
+import photo6 from "../assets/images/photo6.jpg";
+import photo7 from "../assets/images/photo7.jpg";
+import photo8 from "../assets/images/photo8.jpg";
+import home1 from "../assets/images/home1.jpg";
+import home2 from "../assets/images/home2.jpg";
+import home3 from "../assets/images/home3.jpg";
+import home4 from "../assets/images/home4.jpg";
 
 const Insta = () => {
   return (
     <div className="relative p-4 mb-4">
-      <div className="flex justify-center items-center  p-3">
-        <button className="mt-[90px] mb-4 bg-orange-200 hover:bg-blue-600 px-4 py-1 rounded-2xl text-orange-500 hover:text-black h-8 w-28 font-black text-xs uppercase cursor-pointer ">
-          instagram
+      <div className="flex justify-center items-center p-3">
+        <button className="mt-20 mb-4 bg-orange-200 hover:bg-blue-600 px-4 py-1 rounded-2xl text-orange-500 hover:text-black h-8 w-28 font-black text-xs uppercase cursor-pointer">
+          Photos
         </button>
       </div>
-      <div className="relative ">
+      <div className="relative">
         <div className="flex flex-col justify-center items-center">
-          {/* <p className="text-9xl sm:text-[160px] text-blue-700 opacity-5 font-black text-center z-0 absolute top-0 left-0 w-full uppercase dark:text-white">
-            instagram
-          </p> */}
-          <p className="text-2xl sm:text-4xl text-blue-900 font-bold text-center z-20 relative capitalize mt-[55px] dark:text-gray-300">
-            checkout what our customers shared{" "}
-            <span className="normal-case">#homes</span>
+          <p className="text-2xl sm:text-4xl text-blue-900 font-bold text-center z-20 relative capitalize mt-14 dark:text-gray-300">
+            checkout our Photos
           </p>
         </div>
       </div>
 
       <div className="flex justify-center items-center">
-        <div className="grid sm:grid-cols-4 gap-5 m-5 p-5 mt-20 max-w-[1100px] ">
-          <Image
-            src={instaImg1}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110 ease-in duration-200"
-          />
-          <Image
-            src={instaImg2}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
-          <div className="relative ">
-            <Image
-              src={instaImg3}
-              alt="instagram image"
-              className="mx-5 my-3 w-[255px] h-[255px]  rounded-lg hover:scale-110  ease-in duration-200"
-            />
-            <div className="absolute top-[5px] left-[5px] mt-[54px] ml-[66px]  p-[30px] flex justify-center items-center ">
-              <div className=" flex justify-center items-center bg-[#00000080]  p-[25px] gap-[10px] w-20 h-20 rounded-[120px] ">
-                <Image src={instaIcon} alt="insta icon" className="w-5 h-5 " />
-              </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-5 mt-20 max-w-[1300px]">
+          {[
+            photo1,
+            photo2,
+            photo3,
+            photo4,
+            photo5,
+            photo6,
+            photo7,
+            photo8,
+            home1,
+            home2,
+            home3,
+            home4,
+          ].map((photo, index) => (
+            <div key={index} className="flex justify-center">
+              <Image
+                src={photo}
+                alt={`Photo ${index + 1}`}
+                className="w-full max-w-xs h-auto rounded-lg hover:scale-110 transition-transform duration-200"
+              />
             </div>
-          </div>
-          <Image
-            src={instaImg4}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
-          <Image
-            src={instaImg5}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
-          <Image
-            src={instaImg6}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
-          <Image
-            src={instaImg7}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
-          <Image
-            src={instaImg8}
-            alt="instagram image"
-            className="mx-5 my-3 w-[255px] h-[255px] rounded-lg hover:scale-110  ease-in duration-200"
-          />
+          ))}
         </div>
       </div>
     </div>
