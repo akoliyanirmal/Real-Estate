97bee774fbabeef5ee65e8ad0c0f9d516122e801
Nirmal <akoliyanirmal43@gmail.com>
2024-07-24 20:41:14 +0530

    second method in photo page

--- src/components/insta.jsx
+++ src/components/insta.jsx
@@ -25,7 +25,7 @@ const Photos = () => {
       <div className="relative">
         <div className="flex flex-col justify-center items-center">
           <p className="text-2xl sm:text-4xl text-blue-900 font-bold text-center z-20 relative capitalize mt-14 dark:text-gray-300">
-            checkout our Photos
+            Checkout our Photos
           </p>
         </div>
       </div>
@@ -44,17 +44,138 @@ const Photos = () => {
             className="flex transition-transform duration-500"
             style={{ transform: `translateX(-${currentSlide * 100}%)` }}
           >
-            {[...Array(12).keys()].map((_, index) => (
-              <div key={index} className="flex-shrink-0 w-full">
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-1">
                 <Image
                   src={banner}
-                  alt={`photo ${index + 1}`}
+                  alt="photo 1"
                   className="w-full h-auto max-h-[600px] object-cover rounded-lg"
                   width={1000}
                   height={600}
                 />
-              </div>
-            ))}
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-2">
+                <Image
+                  src={banner}
+                  alt="photo 2"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-3">
+                <Image
+                  src={banner}
+                  alt="photo 3"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-4">
+                <Image
+                  src={banner}
+                  alt="photo 4"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-5">
+                <Image
+                  src={banner}
+                  alt="photo 5"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-6">
+                <Image
+                  src={banner}
+                  alt="photo 6"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-7">
+                <Image
+                  src={banner}
+                  alt="photo 7"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-8">
+                <Image
+                  src={banner}
+                  alt="photo 8"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-9">
+                <Image
+                  src={banner}
+                  alt="photo 9"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-10">
+                <Image
+                  src={banner}
+                  alt="photo 10"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-11">
+                <Image
+                  src={banner}
+                  alt="photo 11"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
+            <div className="flex-shrink-0 w-full">
+              <a href="link-to-page-12">
+                <Image
+                  src={banner}
+                  alt="photo 12"
+                  className="w-full h-auto max-h-[600px] object-cover rounded-lg"
+                  width={1000}
+                  height={600}
+                />
+              </a>
+            </div>
           </div>
         </div>
 
